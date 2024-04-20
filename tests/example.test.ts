a0e94be8e3e2ba27b9ad8a95cf35f1a6e8342ef3
Chan <qufstar46@gmail.com>
2024-04-21 00:12:56 +0900

    chore: comment out example codes

--- tests/example.test.ts
+++ tests/example.test.ts
@@ -5,31 +5,35 @@ import { waitFor } from "@storybook/test";
 let frame: Frame;
 
 test.beforeEach(async ({ page }) => {
-  await page.goto(
-    "http://localhost:6006/?path=/story/example-page--logged-out"
-  );
-  await expect(page.getByTitle("storybook-preview-iframe")).toBeVisible();
-  frame = page.frame({ url: /http:\/\/localhost:6006\/iframe.html/ })!;
-  await expect(frame).not.toBeNull();
+  // await page.goto(
+  //   "http://localhost:6006/?path=/story/example-page--logged-out",
+  // );
+  // await expect(page.getByTitle("storybook-preview-iframe")).toBeVisible();
+  // frame = page.frame({ url: /http:\/\/localhost:6006\/iframe.html/ })!;
+  // await expect(frame).not.toBeNull();
 });
 
 test("has logout button", async ({ page }) => {
-  const loginButton = frame.getByRole("button", { name: /John/i }).first();
-  await expect(loginButton).toBeVisible();
-  await loginButton.click();
-
-  await expect(
-    frame.getByRole("button", {
-      name: /John/i,
-    })
-  ).toBeVisible();
+  // await page.goto(
+  //   "http://localhost:6006/?path=/story/example-page--logged-out",
+  // );
+  // await expect(page.getByTitle("storybook-preview-iframe")).toBeVisible();
+  // const frame = page.frame({ url: /http:\/\/localhost:6006\/iframe.html/ })!;
+  // await expect(frame).not.toBeNull();
+  // const button = frame.getByRole("button", { name: /John/i }).first();
+  // await expect(button).toBeVisible();
+  // await button.click();
+  // await expect(
+  //   frame.getByRole("button", {
+  //     name: /John/i,
+  //   }),
+  // ).toBeVisible();
 });
 
 test("has mocking enabled", async ({ page }) => {
-  await page.goto("http://localhost:6006/?path=/story/example-button--large");
-  await expect(page.getByTitle("storybook-preview-iframe")).toBeVisible();
-  frame = page.frame({ url: /http:\/\/localhost:6006\/iframe.html/ })!;
-  await expect(frame).not.toBeNull();
-
-  await expect(frame.getByText("John")).toBeVisible();
+  // await page.goto("http://localhost:6006/?path=/story/example-button--large");
+  // await expect(page.getByTitle("storybook-preview-iframe")).toBeVisible();
+  // frame = page.frame({ url: /http:\/\/localhost:6006\/iframe.html/ })!;
+  // await expect(frame).not.toBeNull();
+  // await expect(frame.getByText("John")).toBeVisible();
 });
