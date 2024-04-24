603eaf87874a1c13286d373e62334aaed4e56662
Chan <qufstar46@gmail.com>
2024-04-25 02:59:03 +0900

    chore: add booth-related icons

---
+++ src/shared/ui/DotIcon.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function DotIcon() {
+  return (
+    <svg
+      width="8"
+      height="8"
+      viewBox="0 0 8 8"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle id="Ellipse 214" cx="4" cy="4" r="4" fill="#D9D9D9" />
+    </svg>
+  );
+}
---
+++ src/shared/ui/RedDotIcon.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function RedDotIcon() {
+  return (
+    <svg
+      width="15"
+      height="15"
+      viewBox="0 0 15 15"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle id="Ellipse 222" cx="7.5" cy="7.5" r="7.5" fill="#F5687E" />
+    </svg>
+  );
+}
