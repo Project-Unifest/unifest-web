204a1cec8f33db130842dce8c2ea5a9c5f9e2e14
Chan <qufstar46@gmail.com>
2024-04-26 02:10:46 +0900

    feat: animate ellipsis using AutoAnimate

--- src/widgets/add-booth/ui/SetName.tsx
+++ src/widgets/add-booth/ui/SetName.tsx
@@ -1,25 +1,55 @@
+"use client";
+
+import DotIcon from "@/src/shared/ui/DotIcon";
+import RedDotIcon from "@/src/shared/ui/RedDotIcon";
 import { Input } from "@/src/shared/ui/input";
-import React from "react";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
+import React, { useState } from "react";
 
 export function SetName() {
+  const [colorIds, setColorIds] = useState(["red0", "gray0", "gray1"]);
+  const [parent] = useAutoAnimate();
+
+  const change = () => {
+    const juggled = [...colorIds];
+    juggled.unshift(juggled.pop()!);
+    setColorIds(juggled);
+  };
+
   return (
-    <div className="space-y-[143px]">
-      <div className="flex flex-col items-center justify-start space-y-3">
-        <h2 className="text-lg font-semibold">부스 이름을 입력해주세요</h2>
-        <p className="text-gray text-xs font-medium">
-          이후 부스 편집을 통해 언제든 수정 가능합니다.
-        </p>
-      </div>
-      <div className="space-y-[6px]">
-        <Input
-          className="rounded-none border-0 border-b text-center shadow-none"
-          type="text"
-          placeholder="부스 이름을 입력해주세요"
-        />
-        <div className="flex items-start justify-end">
-          <div className="text-gray text-[10px] font-medium">0/30자</div>
+    <>
+      <div className="mx-auto mb-[46px] w-[80px]">
+        <div
+          ref={parent}
+          className="flex h-[15px] w-[70px] items-center justify-between"
+        >
+          {colorIds.map((colorId) =>
+            colorId.includes("red") ? (
+              <RedDotIcon key={colorId} />
+            ) : (
+              <DotIcon key={colorId} />
+            ),
+          )}
         </div>
       </div>
-    </div>
+      <div className="space-y-[143px]">
+        <div className="flex flex-col items-center justify-start space-y-3">
+          <h2 className="text-lg font-semibold">부스 이름을 입력해주세요</h2>
+          <p className="text-xs font-medium text-gray">
+            이후 부스 편집을 통해 언제든 수정 가능합니다.
+          </p>
+        </div>
+        <div className="space-y-[6px]">
+          <Input
+            className="rounded-none border-0 border-b text-center shadow-none"
+            type="text"
+            placeholder="부스 이름을 입력해주세요"
+          />
+          <div className="flex items-start justify-end">
+            <div className="text-[10px] font-medium text-gray">0/30자</div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
