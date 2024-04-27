1f0e38dadc8c013b508d6743603da2fc31378020
Chan <qufstar46@gmail.com>
2024-04-28 01:46:12 +0900

    feat: replace local state with global state

--- src/widgets/add-booth/ui/SetCategory.tsx
+++ src/widgets/add-booth/ui/SetCategory.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useBoothStore } from "@/src/shared/model/provider/booth-store-provider";
 import AlcoholIcon from "@/src/shared/ui/AlcoholIcon";
 import { Button } from "@/src/shared/ui/Button";
 import CircleEllipsisIcon from "@/src/shared/ui/CircleEllipsisIcon";
@@ -13,8 +14,11 @@ import Link from "next/link";
 import React, { useState } from "react";
 
 export function SetCategory() {
-  const [selectedCategory, setSelectedCategory] = useState("");
-  const isCategorySelected = Boolean(selectedCategory);
+  const [category, editCategory] = useBoothStore((state) => [
+    state.category,
+    state.editCategory,
+  ]);
+  const isCategorySelected = Boolean(category);
 
   const [parent] = useAutoAnimate();
 
@@ -28,19 +32,19 @@ export function SetCategory() {
       </div>
       <RadioGroup
         className="grid auto-rows-[173px] grid-cols-2 gap-x-[17px] gap-y-[14px]"
-        value={selectedCategory}
-        onValueChange={setSelectedCategory}
+        value={category}
+        onValueChange={editCategory}
       >
         <Label
           htmlFor="bar"
-          className={`flex cursor-pointer flex-col items-center justify-between rounded-xl border pb-7 pt-12 ${selectedCategory === "bar" ? "border-pink" : "border-[#B7B7B7]"}`}
+          className={`flex cursor-pointer flex-col items-center justify-between rounded-xl border pb-7 pt-12 ${category === "bar" ? "border-pink" : "border-[#B7B7B7]"}`}
         >
           <RadioGroupItem className="sr-only" value="bar" id="bar" />
           <AlcoholIcon
-            className={`${selectedCategory === "bar" ? "text-pink" : "text-[#b2b2b2]"}`}
+            className={`${category === "bar" ? "text-pink" : "text-[#b2b2b2]"}`}
           />
           <div
-            className={`text-base font-bold ${selectedCategory === "bar" ? "text-pink" : "text-[#A7A7A7]"}`}
+            className={`text-base font-bold ${category === "bar" ? "text-pink" : "text-[#A7A7A7]"}`}
           >
             주점
           </div>
@@ -48,33 +52,33 @@ export function SetCategory() {
 
         <Label
           htmlFor="food"
-          className={`flex cursor-pointer flex-col items-center justify-between rounded-xl border pb-7 pt-12 ${selectedCategory === "food" ? "border-pink" : "border-[#B7B7B7]"}`}
+          className={`flex cursor-pointer flex-col items-center justify-between rounded-xl border pb-7 pt-12 ${category === "food" ? "border-pink" : "border-[#B7B7B7]"}`}
         >
           <RadioGroupItem className="sr-only" value="food" id="food" />
           <FoodIcon
-            className={`${selectedCategory === "food" ? "text-pink" : "text-[#b2b2b2]"}`}
+            className={`${category === "food" ? "text-pink" : "text-[#b2b2b2]"}`}
           />
           <div className="text-base font-bold text-[#A7A7A7]">먹거리</div>
         </Label>
 
         <Label
           htmlFor="event"
-          className={`flex cursor-pointer flex-col items-center justify-between rounded-xl border pb-7 pt-7 ${selectedCategory === "event" ? "border-pink" : "border-[#B7B7B7]"}`}
+          className={`flex cursor-pointer flex-col items-center justify-between rounded-xl border pb-7 pt-7 ${category === "event" ? "border-pink" : "border-[#B7B7B7]"}`}
         >
           <RadioGroupItem className="sr-only" value="event" id="event" />
           <EventIcon
-            className={`${selectedCategory === "event" ? "text-pink" : "text-[#b2b2b2]"}`}
+            className={`${category === "event" ? "text-pink" : "text-[#b2b2b2]"}`}
           />
           <div className="text-base font-bold text-[#A7A7A7]">이벤트</div>
         </Label>
 
         <Label
           htmlFor="more"
-          className={`flex cursor-pointer flex-col items-center justify-between rounded-xl border pb-7 pt-9 ${selectedCategory === "more" ? "border-pink" : "border-[#B7B7B7]"}`}
+          className={`flex cursor-pointer flex-col items-center justify-between rounded-xl border pb-7 pt-9 ${category === "more" ? "border-pink" : "border-[#B7B7B7]"}`}
         >
           <RadioGroupItem className="sr-only" value="more" id="more" />
           <CircleEllipsisIcon
-            className={`${selectedCategory === "more" ? "text-pink" : "text-[#b2b2b2]"}`}
+            className={`${category === "more" ? "text-pink" : "text-[#b2b2b2]"}`}
           />
           <div className="text-base font-bold text-[#A7A7A7]">일반</div>
         </Label>
@@ -85,10 +89,8 @@ export function SetCategory() {
         ref={parent}
       >
         {isCategorySelected && (
-          <Button
-            className="w-full rounded-[10px] bg-pink py-3 text-white hover:bg-pink"
-            asChild
-          >
+          <Button className="w-full rounded-[10px] bg-pink py-3 text-white hover:bg-pink">
+            {/* TODO send booth data to the server */}
             <Link href="/add-booth/set-description">입력완료</Link>
           </Button>
         )}
