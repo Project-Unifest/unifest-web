b6a2a0c792288086b544fc6146c65c121b734e8b
Chan <qufstar46@gmail.com>
2024-04-29 14:32:41 +0900

    feat: persist position in global state

--- src/shared/model/store/booth-store.ts
+++ src/shared/model/store/booth-store.ts
@@ -1,25 +1,41 @@
 import { devtools, persist } from "zustand/middleware";
 import { createStore } from "zustand/vanilla";
 
+export interface Position {
+  latitude: number;
+  longitude: number;
+}
+
 export type BoothState = {
   name: string;
   category: string;
   description: string;
+  position: Position;
 };
 
 export type BoothActions = {
   editName: (newName: string) => void;
   editCategory: (newCategory: string) => void;
   editDescription: (newDescription: string) => void;
+  editPosition: (newPosition: Position) => void;
 };
 
 export type BoothStore = BoothState & BoothActions;
 
-export const defaultInitState: BoothState = {
+export enum CampusPosition {
+  latitude = 37.542352,
+  longitude = 127.076824,
+}
+
+export const defaultInitState = {
   name: "",
   category: "",
   description: "",
-};
+  position: {
+    latitude: CampusPosition.latitude,
+    longitude: CampusPosition.longitude,
+  },
+} satisfies BoothState;
 
 export const createBoothStore = (initState: BoothState = defaultInitState) => {
   return createStore<BoothStore>()(
@@ -32,6 +48,8 @@ export const createBoothStore = (initState: BoothState = defaultInitState) => {
             set((state) => ({ ...state, category: newCategory })),
           editDescription: (newDescription) =>
             set((state) => ({ ...state, description: newDescription })),
+          editPosition: (newPosition) =>
+            set((state) => ({ ...state, position: { ...newPosition } })),
         }),
         {
           name: "booth-storage",
--- src/widgets/add-booth/index.ts
+++ src/widgets/add-booth/index.ts
@@ -1,4 +1,4 @@
-export { Map as OverviewBoothMap } from "./ui/Map";
+export { GoogleMap as OverviewBoothMap } from "./ui/Map";
 export { SelectMode } from "./ui/SelectMode";
 export { SetCategory as OverviewSetBoothCategory } from "./ui/SetCategory";
 export { SetDescription as OverviewSetBoothDescription } from "./ui/SetDescription";
