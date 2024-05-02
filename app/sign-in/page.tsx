2c886cf46fe8401b78b44563b8681e57301a21fc
Chan <qufstar46@gmail.com>
2024-05-02 13:19:47 +0900

    fix: apply changed filename

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,6 +1,6 @@
-import { SignIn } from "@/src/widgets/sign-in";
+import { SignInForm } from "@/src/widgets/sign-in";
 import React from "react";
 
 export default function SingInPage() {
-  return <SignIn />;
+  return <SignInForm />;
 }
