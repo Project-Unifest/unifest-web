40033133506a49853de0f47a04d58174db4d7483
Chan <qufstar46@gmail.com>
2024-05-02 13:18:06 +0900

    feat: add booth-edit page

---
+++ src/entities/menu/ui/Menu.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function Menu() {
+  return <div>Menu</div>;
+}
--- src/features/booth/index.ts
+++ src/features/booth/index.ts
@@ -1,3 +1,7 @@
+export { EditImageBox as BoothEditImageBox } from "./ui/EditImageBox";
+export { EditMenuBox as BoothEditMenuBox } from "./ui/EditMenuBox";
+export { EditTextBox as BoothEditTextBox } from "./ui/EditTextBox";
+
 export { DeleteButton as BoothDeleteButton } from "./ui/DeleteButton";
 export { EditButton as BoothEditButton } from "./ui/EditButton";
 export { SwitchButton as BoothSwitchButton } from "./ui/SwitchButton";
