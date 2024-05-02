3dc948e79591c782b62c7def49978816ef4006fa
Chan <qufstar46@gmail.com>
2024-05-02 15:23:57 +0900

    test: replace role query with placeholder query

--- stories/SignInForm.stories.tsx
+++ stories/SignInForm.stories.tsx
@@ -1,6 +1,6 @@
 import { SignInForm } from "@/src/widgets/sign-in";
 import { Meta, StoryObj } from "@storybook/react";
-import { expect, fireEvent, userEvent, within } from "@storybook/test";
+import { expect, fireEvent, userEvent, waitFor, within } from "@storybook/test";
 
 const meta = {
   title: "Widgets/SignIn/SignInForm",
@@ -18,7 +18,7 @@ export const Submit: Story = {
   play: async ({ canvasElement }) => {
     const canvas = within(canvasElement);
     await userEvent.type(
-      canvas.getByRole("textbox", { name: "아이디" }),
+      canvas.getByPlaceholderText("아이디를 입력해주세요"),
       "abcd@efgh.com",
     );
     await userEvent.type(
@@ -26,8 +26,10 @@ export const Submit: Story = {
       "12345678",
     );
 
-    await expect(
-      canvas.getByRole("button", { name: "로그인" }),
-    ).toBeInTheDocument();
+    await waitFor(() =>
+      expect(
+        canvas.getByRole("button", { name: "로그인" }),
+      ).toBeInTheDocument(),
+    );
   },
 };
