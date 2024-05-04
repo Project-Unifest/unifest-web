d06ea13b5d4b3a56e1ba4d8dcbc3797b4408046e
Chan <qufstar46@gmail.com>
2024-05-05 03:13:45 +0900

    fix: run effect after authStorage has been hydrated from localStorage

---
+++ src/shared/model/auth/useRequireAuth.ts
@@ -0,0 +1,38 @@
+import React, { useEffect } from "react";
+import { useAuthStore } from "../provider/auth-store-provider";
+import { useRouter } from "next/navigation";
+
+export enum AuthType {
+  GUEST = "guest",
+  MEMBER = "member",
+}
+
+export default function useRequireAuth(requiredAuthType: AuthType) {
+  const [accessToken, refreshToken, isHydrated] = useAuthStore((state) => [
+    state.accessToken,
+    state.refreshToken,
+    state.isHydrated,
+  ]);
+
+  const currentAuthType = (accessToken && AuthType.MEMBER) || AuthType.GUEST;
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!isHydrated) {
+      return;
+    }
+
+    if (
+      requiredAuthType === AuthType.MEMBER &&
+      requiredAuthType !== currentAuthType
+    ) {
+      router.push("/sign-in");
+    }
+    if (
+      requiredAuthType === AuthType.GUEST &&
+      requiredAuthType !== currentAuthType
+    ) {
+      router.push("/");
+    }
+  }, [router, currentAuthType, requiredAuthType, isHydrated]);
+}
