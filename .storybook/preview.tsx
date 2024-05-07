34c5727a8330d34eedc7cee7d0e6325af410af30
Chan <qufstar46@gmail.com>
2024-05-08 01:20:08 +0900

    feat: add booth edit store

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -3,6 +3,7 @@ import type { Preview } from "@storybook/react";
 import { initialize, mswLoader } from "msw-storybook-addon";
 import "../app/globals.css";
 import "./preview.css";
+import { BoothEditStoreProvider } from "../src/shared/model/provider/booth-edit-store.provider";
 import { BoothDraftStoreProvider } from "../src/shared/model/provider/booth-draft-store-provider";
 import { BoothListStoreProvider } from "../src/shared/model/provider/booth-list-store-provider";
 import { AuthStoreProvider } from "../src/shared/model/provider/auth-store-provider";
@@ -18,13 +19,15 @@ const preview: Preview = {
     (Story) => {
       return (
         <AuthStoreProvider>
-          <BoothDraftStoreProvider>
-            <BoothListStoreProvider>
-              <div style={{ fontFamily: "Pretendard, sans-serif" }}>
-                <Story />
-              </div>
-            </BoothListStoreProvider>
-          </BoothDraftStoreProvider>
+          <BoothEditStoreProvider>
+            <BoothDraftStoreProvider>
+              <BoothListStoreProvider>
+                <div style={{ fontFamily: "Pretendard, sans-serif" }}>
+                  <Story />
+                </div>
+              </BoothListStoreProvider>
+            </BoothDraftStoreProvider>
+          </BoothEditStoreProvider>
         </AuthStoreProvider>
       );
     },
