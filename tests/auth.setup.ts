b2326fc282446d1adc488fd4e11774f553ecd109
Chan <qufstar46@gmail.com>
2024-05-10 18:28:22 +0900

    debug: check naviation on authentication

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -11,6 +11,9 @@ setup("authenticate", async ({ page }) => {
     .getByPlaceholder("비밀번호를 입력해주세요")
     .fill(process.env.VERIFIED_ACCOUNT_PASSWORD!);
   await page.getByRole("button", { name: "로그인" }).click();
+  console.log(process.env.VERIFIED_ACCOUNT_ID!);
+  console.log(process.env.VERIFIED_ACCOUNT_PASSWORD!);
+  console.log(page.url());
   await page.waitForURL("/");
   await expect(page.getByText("운영중인 부스 없음")).not.toBeVisible();
 
