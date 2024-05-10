26f0dd2956114676e2827d932b4b372fc7fc3c98
Chan <qufstar46@gmail.com>
2024-05-10 18:48:55 +0900

    debug: check env variables

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -10,6 +10,8 @@ setup("authenticate", async ({ page }) => {
   await page
     .getByPlaceholder("비밀번호를 입력해주세요")
     .fill(process.env.VERIFIED_ACCOUNT_PASSWORD!);
+  console.log(process.env.VERIFIED_ACCOUNT_ID);
+  console.log(process.env.VERIFIED_ACCOUNT_PASSWORD);
   await page.getByRole("button", { name: "로그인" }).click();
   await page.waitForURL("/");
   await expect(page.getByText("운영중인 부스 없음")).not.toBeVisible();
