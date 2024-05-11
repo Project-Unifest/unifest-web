ffc809e6ddf3d2345298f73fb9145e1df617e489
Chan <qufstar46@gmail.com>
2024-05-11 19:33:23 +0900

    feat: toggle booth opened state

--- src/features/booth/ui/SwitchButton.tsx
+++ src/features/booth/ui/SwitchButton.tsx
@@ -1,21 +1,31 @@
 "use client";
 
+import useAuthFetch from "@/src/shared/model/auth/useAuthFetchList";
 import { Label } from "@/src/shared/ui/label";
 import { Switch } from "@/src/shared/ui/switch";
 import React, { useState } from "react";
+import { updateBoothOpened } from "../api/booth";
 
-export function SwitchButton() {
-  const [isOpened, setIsOpened] = useState<boolean>(false);
+export function SwitchButton({
+  boothId,
+  initialOpened,
+}: Readonly<{ boothId: number; initialOpened?: boolean }>) {
+  const [opened, setIsOpened] = useState<boolean>(false);
+
+  const updateAuthBoothOpened = useAuthFetch(updateBoothOpened);
+
+  const toggleBoothOpened = async () => {
+    await updateAuthBoothOpened(boothId, !opened);
+    setIsOpened((currentOpened) => !currentOpened);
+  };
 
   return (
     <div className="flex items-center space-x-2">
       <Label htmlFor="booth-mode">영업</Label>
       <Switch
         id="booth-mode"
-        checked={isOpened}
-        onCheckedChange={() =>
-          setIsOpened((currentIsOpened) => !currentIsOpened)
-        }
+        checked={opened}
+        onCheckedChange={toggleBoothOpened}
       />
     </div>
   );
