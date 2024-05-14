946c71d4b136d980e91b2654bb69d6d3dda4955c
Chan <qufstar46@gmail.com>
2024-05-15 01:40:36 +0900

    feat: add signout

---
+++ src/shared/ui/LogoutIcon.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function LogoutIcon() {
+  return (
+    <svg
+      width="24"
+      height="21"
+      viewBox="0 0 24 21"
+      fill="currentColor"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M17 6.125L15.59 7.35875L18.17 9.625H8V11.375H18.17L15.59 13.6325L17 14.875L22 10.5M4 4.375H12V2.625H4C2.9 2.625 2 3.4125 2 4.375V16.625C2 17.5875 2.9 18.375 4 18.375H12V16.625H4V4.375Z"
+        fill="currentColor"
+      />
+    </svg>
+  );
+}
