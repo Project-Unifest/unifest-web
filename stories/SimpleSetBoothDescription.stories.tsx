14b60af8aa5bfbaa42762f28881ddf170d4918a2
Chan <qufstar46@gmail.com>
2024-07-07 18:13:33 +0000

    test: check description length while overtyped

--- stories/SimpleSetBoothDescription.stories.tsx
+++ stories/SimpleSetBoothDescription.stories.tsx
@@ -32,12 +32,33 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {};
 
-export const DescriptionTyped: Story = {
+export const DescriptionEmpty: Story = {
   play: async ({ canvasElement }) => {
     const canvas = within(canvasElement);
     await expect(
       canvas.getByRole("button", { name: "건너뛰기" }),
     ).toBeInTheDocument();
+
+    await expect(
+      canvas.getByPlaceholderText(
+        /부스 컨텐츠, 판매 음식 등 자유롭게 부스를 소개해보세요/,
+      ),
+    ).toBeInTheDocument();
+
+    await expect(
+      canvas.getByRole("button", { name: "건너뛰기" }),
+    ).toBeInTheDocument();
+
+    await expect(
+      canvas.queryByRole("button", { name: "입력완료" }),
+    ).not.toBeInTheDocument();
+  },
+};
+
+export const DescriptionTyped: Story = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
     await userEvent.type(
       canvas.getByPlaceholderText(
         /부스 컨텐츠, 판매 음식 등 자유롭게 부스를 소개해보세요/,
@@ -50,5 +71,25 @@ export const DescriptionTyped: Story = {
     await expect(
       await canvas.findByRole("button", { name: "입력완료" }),
     ).toBeInTheDocument();
+  },
+};
+
+export const DescriptionOvertyping: Story = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    const descriptionTextField = canvas.getByPlaceholderText(
+      /부스 컨텐츠, 판매 음식 등 자유롭게 부스를 소개해보세요/,
+    );
+
+    await userEvent.type(
+      canvas.getByPlaceholderText(
+        /부스 컨텐츠, 판매 음식 등 자유롭게 부스를 소개해보세요/,
+      ),
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    );
+    await expect(descriptionTextField.textContent?.length).toBeLessThanOrEqual(
+      100,
+    );
   },
 };
