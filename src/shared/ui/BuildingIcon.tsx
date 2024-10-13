f1bcb51edb82fa2a0a78986d0beec90516cbe0c4
Chan <qufstar46@gmail.com>
2024-10-13 09:55:03 +0000

    feat: change labels and icons on simple booth creation page

---
+++ src/shared/ui/BuildingIcon.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+export default function BuildingIcon(props: { className?: string }) {
+  return (
+    <svg
+      width="62"
+      height="53"
+      viewBox="0 0 62 53"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <rect
+        x="17"
+        y="2"
+        width="28"
+        height="49"
+        rx="2.16129"
+        stroke="currentColor"
+        stroke-width="3.24194"
+      />
+      <path
+        d="M45 19.1613C45 17.9676 45.9676 17 47.1613 17H57.8387C59.0324 17 60 17.9676 60 19.1613V48.8387C60 50.0324 59.0324 51 57.8387 51H45V19.1613Z"
+        stroke="currentColor"
+        stroke-width="3.24194"
+      />
+      <path
+        d="M2 35.7414C2 34.5477 2.96764 33.5801 4.16129 33.5801H14.9677C16.1614 33.5801 17.129 34.5477 17.129 35.7414V50.8704H4.16129C2.96764 50.8704 2 49.9028 2 48.7091V35.7414Z"
+        stroke="currentColor"
+        stroke-width="3.24194"
+      />
+      <rect x="23" y="8" width="5" height="6" rx="1" fill="currentColor" />
+      <rect x="23" y="20" width="5" height="6" rx="1" fill="currentColor" />
+      <rect x="23" y="32" width="5" height="6" rx="1" fill="currentColor" />
+      <rect x="33" y="8" width="5" height="6" rx="1" fill="currentColor" />
+      <rect x="33" y="20" width="5" height="6" rx="1" fill="currentColor" />
+      <rect x="33" y="32" width="5" height="6" rx="1" fill="currentColor" />
+    </svg>
+  );
+}
