da3255531a9e4152cc30f74072f97e8b592c8da3
Chan <qufstar46@gmail.com>
2024-10-27 07:12:07 +0000

    test: alert pops up when user cancels

--- mocks/api/queue.ts
+++ mocks/api/queue.ts
@@ -95,7 +95,7 @@ const queueHandler = [
 
 let countRequest = 0;
 
-export const queueUserCallHandler = [
+export const queueUserCancelsHandler = [
   http.get(`${API_URL}/waiting/:boothId/all`, ({ params }) => {
     countRequest++;
     const boothId = parseInt(params.boothId as string);
@@ -135,6 +135,103 @@ export const queueUserCallHandler = [
     const id = parseInt(params.id as string);
     const groupToBeCalled = allGroups.get(id);
 
+    if (id === TEST_QUEUE_GROUP_USER_CANCELS) {
+      return new HttpResponse("Not found", {
+        status: 404,
+      });
+    }
+
+    if (groupToBeCalled) {
+      allGroups.set(id, { ...groupToBeCalled, status: "CALLED" });
+      return HttpResponse.json({
+        code: "200",
+        message: "성공",
+        data: groupToBeCalled,
+      });
+    }
+
+    return HttpResponse.json("Not found", {
+      status: 404,
+    });
+  }),
+  http.put(`${API_URL}/waiting/:id/complete`, ({ params }) => {
+    const id = parseInt(params.id as string);
+    const groupToBeCompleted = allGroups.get(id);
+
+    if (id === TEST_QUEUE_GROUP_USER_CANCELS) {
+      return new HttpResponse("Not found", {
+        status: 404,
+      });
+    }
+
+    if (groupToBeCompleted) {
+      allGroups.set(id, { ...groupToBeCompleted, status: "COMPLETED" });
+      return HttpResponse.json({});
+    }
+
+    return HttpResponse.json("Not found", {
+      status: 404,
+    });
+  }),
+  http.delete(`${API_URL}/waiting/:id`, ({ params }) => {
+    const id = parseInt(params.id as string);
+
+    const isGroupDeleted = allGroups.delete(id);
+
+    if (isGroupDeleted) {
+      return HttpResponse.json({
+        code: "200",
+        message: "성공",
+      });
+    }
+
+    return new HttpResponse("Not found", {
+      status: 404,
+    });
+  }),
+];
+
+export const queueUserCancelsBeforeAdminHandler = [
+  http.get(`${API_URL}/waiting/:boothId/all`, ({ params }) => {
+    countRequest++;
+    const boothId = parseInt(params.boothId as string);
+
+    if (boothId === 0) {
+      if (countRequest > TEST_THRESHOLD) {
+        return HttpResponse.json(
+          wrapResponse(
+            2000,
+            "OK",
+            Array.from(allGroups.values()).filter(
+              (group) => group.waitingId !== TEST_QUEUE_GROUP_USER_CANCELS,
+            ),
+          ),
+        );
+      }
+      return HttpResponse.json(
+        wrapResponse(2000, "OK", Array.from(allGroups.values())),
+      );
+    }
+
+    if (boothId === 1) {
+      return HttpResponse.json([]);
+    }
+
+    return new HttpResponse("Not found", {
+      status: 404,
+    });
+  }),
+  http.put(`${API_URL}/waiting/:id/call`, ({ params }) => {
+    const id = parseInt(params.id as string);
+
+    if (id === TEST_QUEUE_GROUP_USER_CANCELS) {
+      return new HttpResponse("Not found", {
+        status: 404,
+      });
+    }
+
+    const groupToBeCalled = allGroups.get(id);
+
     if (groupToBeCalled) {
       allGroups.set(id, { ...groupToBeCalled, status: "CALLED" });
       return HttpResponse.json({
@@ -163,6 +260,12 @@ export const queueUserCallHandler = [
   }),
   http.delete(`${API_URL}/waiting/:id`, ({ params }) => {
     const id = parseInt(params.id as string);
+
+    if (id === TEST_QUEUE_GROUP_USER_CANCELS) {
+      return new HttpResponse("Not found", {
+        status: 404,
+      });
+    }
 
     const isGroupDeleted = allGroups.delete(id);
 
