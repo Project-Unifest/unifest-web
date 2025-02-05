fbe9b53d31664b59be37e3c7dd150585a4c5350f
Byeolchan Kim <qufstar46@gmail.com>
2025-02-06 01:38:10 +0900

    refactor: use ky for megaphone apis

--- src/features/megaphone/api/megaphone.ts
+++ src/features/megaphone/api/megaphone.ts
@@ -1,27 +1,15 @@
-import {
-  API_URL,
-  HTTPHeaderKey,
-  HTTPHeaderValue,
-  HTTPMethod,
-  getAuthorziationValue,
-} from "@/src/shared/api/config";
+import { client } from "@/src/shared/api/client";
 
 export const makeMegaphone = async (
-  accessToken: string,
   boothId: number,
   msg: { msgBody: string },
 ) => {
-  const response = await fetch(`${API_URL}/megaphone`, {
-    method: HTTPMethod.POST,
-    headers: {
-      [`${HTTPHeaderKey.CONTENT_TYPE}`]: HTTPHeaderValue.APPLICATION_JSON,
-      [`${HTTPHeaderKey.AUTHORIZATION}`]: getAuthorziationValue(accessToken),
-    },
-    body: JSON.stringify({
-      ...msg,
-      boothId,
-    }),
-  });
-  const data = await response.json();
-  return data;
+  return client
+    .post("megaphone", {
+      json: {
+        ...msg,
+        boothId,
+      },
+    })
+    .json();
 };
