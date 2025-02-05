c38a8ce1519e1cea8fe31521211f68fea318d9d9
Byeolchan Kim <qufstar46@gmail.com>
2025-02-06 00:56:58 +0900

    refactor: use ky in booth related apis

--- src/features/booth/api/image.ts
+++ src/features/booth/api/image.ts
@@ -1,18 +1,12 @@
-import {
-  API_URL,
-  HTTPHeaderKey,
-  HTTPHeaderValue,
-  HTTPMethod,
-} from "@/src/shared/api/config";
+import { publicClient } from "@/src/shared/api/client";
 
 export const uploadImage = async (image: File) => {
   const formData = new FormData();
   formData.append("file", image);
 
-  const response = await fetch(`${API_URL}/images`, {
-    method: HTTPMethod.POST,
-    body: formData,
-  });
-  const data = response.json();
-  return data;
+  return publicClient
+    .post("images", {
+      body: formData,
+    })
+    .json();
 };
