75092f40cf46720778a77dfa7f0bed796cc9d136
Byeolchan Kim <qufstar46@gmail.com>
2025-04-13 21:00:19 +0900

    infra: set up react query devtools on development

--- src/shared/model/provider/query-provider.tsx
+++ src/shared/model/provider/query-provider.tsx
@@ -2,7 +2,7 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactNode, useState } from "react";
-
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 interface QueryProviderProps {
   children: ReactNode;
 }
@@ -24,6 +24,9 @@ export function QueryProvider({ children }: QueryProviderProps) {
   );
 
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 }
