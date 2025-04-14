6ed23840d2d176b0931b4207c9840d74b0ec8749
Byeolchan Kim <qufstar46@gmail.com>
2025-04-15 02:17:23 +0900

    storybook: set up query provider

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -3,9 +3,9 @@ import type { Preview } from "@storybook/react";
 import { initialize, mswLoader } from "msw-storybook-addon";
 import "../app/globals.css";
 import "./preview.css";
-
+import { QueryProvider } from "../src/shared/model/provider/query-provider";
 import handlers from "../mocks/api/handlers";
-
+import { MSWProvider } from "../src/app/ui/MSWProvider";
 initialize();
 
 // TODO use next/font/local instead of global style
@@ -16,7 +16,11 @@ const preview: Preview = {
     (Story) => {
       return (
         <div style={{ fontFamily: "Pretendard, sans-serif" }}>
-          <Story />
+          <MSWProvider>
+            <QueryProvider>
+              <Story />
+            </QueryProvider>
+          </MSWProvider>
         </div>
       );
     },
